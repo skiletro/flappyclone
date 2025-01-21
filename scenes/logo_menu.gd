38b2a320f84dde887d334efeffef0a937ab51b4e
skiletro <19377854+skiletro@users.noreply.github.com>
2025-01-21 21:37:17 +0000

    fix: menu bob now working

--- scenes/logo_menu.gd
+++ scenes/logo_menu.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var original_position = position.y
 @export var float_height = 10
-var unsmoothed_position = float_height
 var velocity = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -13,9 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	# Reverse the position when it meets the boundaries
-	if unsmoothed_position > float_height or unsmoothed_position < -float_height:
+	if position.y >= (original_position + float_height) or position.y <= (original_position - float_height):
 		velocity = -velocity
-		
-	unsmoothed_position += velocity * (delta * 15)
 	
-	position.y = original_position + unsmoothed_position
+	position.y += velocity * (delta * 10)
